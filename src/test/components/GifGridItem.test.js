9466a98fc1f7683e8aa7feee5843b6bb9746d56a
Pedro Yanez <pedro_y91@outlook.com>
2022-04-08 11:57:39 -0400

    91 pruebas del componente GifGripItem

--- src/test/components/GifGridItem.test.js
+++ src/test/components/GifGridItem.test.js
@@ -5,9 +5,26 @@ describe (`Pruebas en <GifGridItem />`, () => {
 
 const title = 'Un titulo';
 const url = 'https://localhost/algo.jpg';
+const wrapper = shallow(<GifGridItem title={title} url={url}/>);
 
   test('Debe de mostrar el componente correctamente', () => {
-    const wrapper = shallow(<GifGridItem title={title} url={url}/>);
     expect (wrapper).toMatchSnapshot();
   });
+
+  test('Debe de tener un parrafo con el title', () => {
+    const p = wrapper.find('p');
+    expect (p.text().trim()).toBe(title);
+  });
+
+  test('Debe de tener una imagen con la url y alt de los props', () => {
+    const img = wrapper.find('img');
+    expect (img.prop('src')).toBe(url);
+    expect (img.prop('alt')).toBe(title);
+  });
+
+  test('Debe de tener la clase animate__animated', () => {
+    const div = wrapper.find('div');
+    expect (div.hasClass('animate__animated')).toBe(true);
+  });
+
 });
