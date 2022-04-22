79ee6806f7787147981af5ec23837942e4b1ecd8
Pedro Yanez <pedro_y91@outlook.com>
2022-04-22 11:59:36 -0400

    96/Simular sumit del form

--- src/test/components/AddCategory.test.js
+++ src/test/components/AddCategory.test.js
@@ -1,13 +1,34 @@
+import '@testing-library/jest-dom';
 import {shallow} from 'enzyme';
 import {AddCategory} from '../../components/AddCategory';
 
 describe('Pruebas en el componente <AddCategory />', () => {
 
-  const setCategories = () => {};
+  const setCategories = jest.fn();
+  let wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+  });
 
   test('Debe de mostrarse correctamente', () => {
-    const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Debe de cambiar la caja de texto', () => {
+    const input = wrapper.find('input');
+    const value = 'Hola Mundo';
+    input.simulate('change', {target: {value}});
+    expect(wrapper.find('input').prop('value')).toBe(value);
+  });
+
+  test('No debe de postear la información con una longitud menor a 3 caracteres', () => {
+    const value = 'Hi';
+    wrapper.find('input').simulate('change', {target: {value}});
+    wrapper.find('form').simulate('submit', {preventDefault() {}});
+    expect(setCategories).not.toHaveBeenCalled();
   });
 
 });
